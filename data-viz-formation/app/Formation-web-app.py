9870aa67d8f176b394bb318cd4a2c328a5f9203f
Jérémie Peres <jeremieperes@gmail.com>
2019-12-06 12:42:55 +0100

    Changement des mot-clés et adaptation aux changements effectués sur le moteur de recherche de MCF (quadrillage de la France)

--- data-viz-formation/app/Formation-web-app.py
+++ data-viz-formation/app/Formation-web-app.py
@@ -13,12 +13,17 @@ import re
 st.title('Web-app Mon Compte Formation')
 
 # Liste de toutes les formations qui nous intéressent
-formations = ["technicien d'etudes du batiment en dessin de projet",
-              "infographiste metteur en page",
-              "developpeur web et web mobile",
-              "designer web",
+formations = ["dessinateur btp",
+              "infographiste",
+              "developpeur web",
+              "developpeur web mobile",
+              "web",
+              "web 2.0",
+              'web Designer',
+              'referenceur web',
+              "programmation web",
               "concepteur developpeur d'applications",
-              "developpeur java",
+              "java",
               "autocad",
               "revit",
               "archicad",
@@ -32,64 +37,92 @@ formations = ["technicien d'etudes du batiment en dessin de projet",
               "indesign",
               "after effects",
               "premiere pro",
-              "cinema 4D",
+              "4D",
               "light room",
               "wordpress",
               "html",
               "css",
               "prestashop",
               "referencement web",
-              "reseaux sociaux",
-              "trafic web",
+              "gestionnaire media sociaux",
+              "community management",
               "webmarketing",
+              "seo",
               "newsletter",
-              "cyber securite",
-              "marketing digital",
+              "cybersecurite",
+              "animateur communautes web",
+              "redacteur web",
+              "administration site web",
+              "site internet",
+              "infographie web",
               "ui ux designer",
-              "animateur e-commerce",
-              "creation d'un site web",
-              "e-commerce",
-              "tosa office et tosa digital",
+              "e commerce",
+              "office word excel powerpoint ",
               "pcie",
               "clea numerique"]
 
-@st.cache
+# Quadrillage de la France
+cities = ["44000", "75001", "33000", "69001", "72000", "13001", "54000", "59000",
+           "31000", "87000", "18000","20000","10000","14000","35000","80000",
+           "37000","21000","67000","17000","63000","38000","06000","34000",
+           "64100","15000"]
+
+
+
+@st.cache(suppress_st_warning=True)
 def load_data(formations):
     with st.spinner('Please wait : the app is loading data from www.moncompteformation.gouv.fr'):
         all_form = {}
         frames = {}
         formations_new = []
 
+        headers = {
+            'authority': 'www.moncompteformation.gouv.fr',
+            'accept': 'application/json, text/plain, */*',
+            'origin': 'https://www.moncompteformation.gouv.fr',
+            'x-xsrf-token': 'aaeeaab6-8806-4969-a2aa-a614eb26839c',
+            'x-requestid': '38ea305b-56f1-8345-746a-90d5960dc94c',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+            'content-type': 'application/json',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'cors',
+            'referer': 'https://www.moncompteformation.gouv.fr/espace-prive/html/',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+            'cookie': 'JSESSIONID=AEC772680A78C76FF000587B14DE2BF4.xsl6eihm_b4; visid_incap_249257=+Zy/6afORoiAX7OEdT3tms/d1l0AAAAAQUIPAAAAAADLezLhGsc7K6ZSaNLdKcpW; incap_ses_878_249257=YnlZWvllrCdj8HLuT0kvDNDd1l0AAAAA+BLv453AQLioH7tO0D1H7w==; incap_ses_474_249257=05ksAOjsCGxSKxTw+v2TBkHe1l0AAAAASnhsVWmYkPtr2cClG5mS/A==; incap_ses_727_249257=SVlUOKnGKAh/RLKb29MWCnXe1l0AAAAAOBs3mEdluJigWM9RBEvx3g==; incap_ses_245_249257=fkuKdY/k3BcbYi0pL2tmA/jk1l0AAAAA5xw4B1M3evgaejdVjuZNJw==; incap_ses_533_249257=K84ZCjjz4lMgE8jrS7JlB37l1l0AAAAA/Xd4ul5uctJScG9078solQ==; incap_ses_535_249257=2puXA1rjWFVQy+eV3bNsB37l1l0AAAAAIuYog7R2LXaAW9I6l65y5A==; incap_ses_875_249257=1XagYUSpcS8cGohN5KAkDH7l1l0AAAAAwRgz1z///KC5zwuwCrkFjg==; incap_ses_473_249257=D/nODwA8Bk1C/xlBkXCQBpzn1l0AAAAAgiM3pkPzfyRNplKGF/XSmQ==; nlbi_249257=ecthUKHeETDxVQjosHLyrwAAAADPx18T6TqNLrTvxH4REMyp; cookie-accepted=1; cookie-agreed=2; incap_ses_730_249257=N7kIAJpOtlLxAZGYO3whCpj81l0AAAAAYsFMaPMWyA+j7FQmB6PkKg==; incap_ses_729_249257=N3m4UbpzR02MPUjp0e4dCpj81l0AAAAAHHNqwNleaefrk2XUBay7EQ==; XSRF-TOKEN=aaeeaab6-8806-4969-a2aa-a614eb26839c; incap_ses_728_249257=0I5UT9eb6nYZf8SmOWEaCrYD110AAAAAVQPlGdmxFk08ciGSEKDupw==; incap_ses_534_249257=VsSmSahkT213UZfoWkBpB7cD110AAAAAbVHIdnvHnbDQYpsijQGxdg==; incap_ses_184_249257=rO2lWCZlM37pg6t5xbSNArcD110AAAAAszOA1TzbbtKID5nRYO9WdA==; incap_ses_408_249257=u2WBOQbYmWAOk64+toOpBcID110AAAAA4eyIEexbzPXnQH01soQE0Q==; incap_ses_876_249257=9zLGSUzhsG8skY+lUi4oDMED110AAAAASvmfK1SLKtI5Fy4NjS9R+g==; incap_ses_246_249257=DkGIRt2P73tGaTGF+vdpA8ID110AAAAAcVeCZpbuzVlWGxgdeCFj1Q==; incap_ses_877_249257=tQB6G5D5whqE34aU6LsrDMID110AAAAAgjj9FFWDOe7LkF4w2XtCVA==',
+        }
+
+
         for formation in formations:
-            headers = {
-                'authority': 'www.moncompteformation.gouv.fr',
-                'accept': 'application/json, text/plain, */*',
-                'origin': 'https://www.moncompteformation.gouv.fr',
-                'x-xsrf-token': 'aaeeaab6-8806-4969-a2aa-a614eb26839c',
-                'x-requestid': '38ea305b-56f1-8345-746a-90d5960dc94c',
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
-                'content-type': 'application/json',
-                'sec-fetch-site': 'same-origin',
-                'sec-fetch-mode': 'cors',
-                'referer': 'https://www.moncompteformation.gouv.fr/espace-prive/html/',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-                'cookie': 'JSESSIONID=AEC772680A78C76FF000587B14DE2BF4.xsl6eihm_b4; visid_incap_249257=+Zy/6afORoiAX7OEdT3tms/d1l0AAAAAQUIPAAAAAADLezLhGsc7K6ZSaNLdKcpW; incap_ses_878_249257=YnlZWvllrCdj8HLuT0kvDNDd1l0AAAAA+BLv453AQLioH7tO0D1H7w==; incap_ses_474_249257=05ksAOjsCGxSKxTw+v2TBkHe1l0AAAAASnhsVWmYkPtr2cClG5mS/A==; incap_ses_727_249257=SVlUOKnGKAh/RLKb29MWCnXe1l0AAAAAOBs3mEdluJigWM9RBEvx3g==; incap_ses_245_249257=fkuKdY/k3BcbYi0pL2tmA/jk1l0AAAAA5xw4B1M3evgaejdVjuZNJw==; incap_ses_533_249257=K84ZCjjz4lMgE8jrS7JlB37l1l0AAAAA/Xd4ul5uctJScG9078solQ==; incap_ses_535_249257=2puXA1rjWFVQy+eV3bNsB37l1l0AAAAAIuYog7R2LXaAW9I6l65y5A==; incap_ses_875_249257=1XagYUSpcS8cGohN5KAkDH7l1l0AAAAAwRgz1z///KC5zwuwCrkFjg==; incap_ses_473_249257=D/nODwA8Bk1C/xlBkXCQBpzn1l0AAAAAgiM3pkPzfyRNplKGF/XSmQ==; nlbi_249257=ecthUKHeETDxVQjosHLyrwAAAADPx18T6TqNLrTvxH4REMyp; cookie-accepted=1; cookie-agreed=2; incap_ses_730_249257=N7kIAJpOtlLxAZGYO3whCpj81l0AAAAAYsFMaPMWyA+j7FQmB6PkKg==; incap_ses_729_249257=N3m4UbpzR02MPUjp0e4dCpj81l0AAAAAHHNqwNleaefrk2XUBay7EQ==; XSRF-TOKEN=aaeeaab6-8806-4969-a2aa-a614eb26839c; incap_ses_728_249257=0I5UT9eb6nYZf8SmOWEaCrYD110AAAAAVQPlGdmxFk08ciGSEKDupw==; incap_ses_534_249257=VsSmSahkT213UZfoWkBpB7cD110AAAAAbVHIdnvHnbDQYpsijQGxdg==; incap_ses_184_249257=rO2lWCZlM37pg6t5xbSNArcD110AAAAAszOA1TzbbtKID5nRYO9WdA==; incap_ses_408_249257=u2WBOQbYmWAOk64+toOpBcID110AAAAA4eyIEexbzPXnQH01soQE0Q==; incap_ses_876_249257=9zLGSUzhsG8skY+lUi4oDMED110AAAAASvmfK1SLKtI5Fy4NjS9R+g==; incap_ses_246_249257=DkGIRt2P73tGaTGF+vdpA8ID110AAAAAcVeCZpbuzVlWGxgdeCFj1Q==; incap_ses_877_249257=tQB6G5D5whqE34aU6LsrDMID110AAAAAgjj9FFWDOe7LkF4w2XtCVA==',
-            }
+            all_form_city = {}
+            for code in cities:
+                data1 = '{"quoi":"%s","ou":{"codePostal":"%s","type":"CP","modality":"0"},"nombreOccurences":10000,"debutPagination":1,"sort":"DISTANCE"}' % (formation, code)
 
-            data = '{"quoi":"%s","ou":{"codePostal":"44000","type":"CP"},"nombreOccurences":10000,"debutPagination":1,"sort":"SCORE"}' % formation
+                response1 = requests.post(
+                    'https://www.moncompteformation.gouv.fr/espace-prive/sl6-portail-web/public/formations',
+                    headers=headers,
+                    data=data1)
 
-            response = requests.post(
+                all_form_city[code] = response1.json()["item"]
+
+            data2 = '{"quoi":"%s","ou":{"modality":"2"},"nombreOccurences":10000,"debutPagination":1,"sort":"SCORE"}' % formation
+
+            response2 = requests.post(
                 'https://www.moncompteformation.gouv.fr/espace-prive/sl6-portail-web/public/formations',
                 headers=headers,
-                data=data)
+                data=data2)
 
-            all_form[formation] = response.json()["item"]
+            all_form_city['distance'] = response2.json()["item"]
 
-            if len(all_form[formation]) != 0:
-                df = pd.DataFrame.from_dict(all_form[formation])
-                df = df[['title', 'organism', 'codePostal', 'ville',
-                         'distance', 'duration', 'prixTotalTTC']]
+            df = pd.DataFrame()
+            for dict_city in all_form_city.values():
+                if len(dict_city)!=0:
+                    df_city = pd.DataFrame.from_dict(dict_city)
+                    df_city = pd.concat([df_city.drop(['id'], axis=1), df_city['id'].apply(pd.Series)], axis=1)
+                    df = pd.concat([df, df_city])
+
+            if len(df)!=0:
+                df = df.drop_duplicates(subset=['title', 'organism', 'codePostal', 'ville', 'duration', 'prixTotalTTC',"numeroFormation","numeroAction"])
                 frames[formation] = df
                 formations_new.append(formation)
 
@@ -104,6 +137,8 @@ def aggregate_df(frames):
             details['Keyword'] = formation
         for df_form in frames_copy.values():
             df = pd.concat([df, df_form])
+        df = df.drop_duplicates(subset=['title', 'organism', 'codePostal', 'ville', 'duration', 'prixTotalTTC',"numeroFormation","numeroAction"])
+        df = df[['title', 'organism', 'codePostal', 'ville','distance', 'duration', 'prixTotalTTC','Keyword']]
     return df
 
 def check_distance_or_not(str):
@@ -141,6 +176,7 @@ def clean_data(df):
             {0: df_copy['duration'].mean()})
         df_copy['Durée'] = df_copy['Durée'].fillna(df_copy['Durée'].mean())
         df_copy['Ville'] = df_copy['Ville'].str.upper()
+        df_copy['Ville'] = df_copy['Ville'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
         df_copy["Ville"] = df_copy["Ville"].str.replace('CEDEX', '')
         df_copy["Ville"] = df_copy["Ville"].str.replace('-', ' ')
         df_copy["Ville"] = df_copy["Ville"].str.replace("'", ' ')
@@ -149,8 +185,6 @@ def clean_data(df):
         df_copy["Ville"] = df_copy["Ville"].str.replace('PARIS.*', 'PARIS')
         df_copy["Ville"] = df_copy["Ville"].str.replace('LYON.*', 'LYON')
         df_copy["Ville"] = df_copy["Ville"].str.replace('MARSEILLE.*', 'MARSEILLE')
-        df_copy["Ville"] = df_copy["Ville"].str.strip()
-        df_copy['Ville'] = df_copy['Ville'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
         df_copy['Ville'] = df_copy['Ville'].str.replace(' ST ',' SAINT ')
         df_copy['Ville'] = df_copy['Ville'].str.replace(' STE ',' SAINTE ')
         df_copy['Ville'] = df_copy['Ville'].str.replace('ST ','SAINT ')
@@ -160,19 +194,26 @@ def clean_data(df):
         df_copy['Ville'] = df_copy['Ville'].str.replace('SOPHIA ANTIPOLIS','NICE')
         df_copy['Ville'] = df_copy['Ville'].str.replace('VERRIERES EN ANJOU','ANGERS')
         df_copy['Ville'] = df_copy['Ville'].str.replace('CHERBOURG EN COTENTIN','CHERBOURG OCTEVILLE')
-        df_copy['Ville'] = df_copy['Ville'].str.replace('FRANCONVILLE LA GARENNE','FRACONVILLE')
+        df_copy['Ville'] = df_copy['Ville'].str.replace('FRANCONVILLE LA GARENNE','FRANCONVILLE')
         df_copy['Ville'] = df_copy['Ville'].str.replace('BEAUPREAU EN MANGES','BEAUPREAU')
         df_copy['Ville'] = df_copy['Ville'].str.replace('ROMAGNY FONTENAY','ROMAGNY')
         df_copy['Ville'] = df_copy['Ville'].str.replace('SAINT JUSAINT SAINT RAMBERT','SAINT JUST SAINT RAMBERT')
-        df_copy['Ville'] = df_copy['Ville'].str.replace('SAINT PRIESAINT EN JAREZ','SAINT PRIEST')
-        df_copy['Ville'] = df_copy['Ville'].str.replace('BAGNOLES DE L ORNE NORMAND','BAGNOLE DE L ORNE')
+        df_copy['Ville'] = df_copy['Ville'].str.replace('SAINT PRIESAINT EN JAREZ','SAINT PRIEST EN JAREZ')
+        df_copy['Ville'] = df_copy['Ville'].str.replace('BAGNOLES DE L ORNE NORMAND','BAGNOLES DE L ORNE')
+        df_copy['Ville'] = df_copy['Ville'].str.replace('LES ACHARDS','ACHARDS')
+        df_copy['Ville'] = df_copy['Ville'].str.replace('ORLY AEROGARE','ORLY')
+        df_copy["Ville"] = df_copy["Ville"].str.strip()
 
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('69465', '69006')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('49480', '49000')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('33701', '33700')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('35407', '35400')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('33401', '33400')
+        df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('34197', '34000')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('33405', '33400')
+        df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('33008', '33000')
+        df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('85016', '85000')
+        df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('95092', '95000')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('29330', '29000')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('35605', '35600')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('^1100$', '01100')
@@ -191,7 +232,9 @@ def clean_data(df):
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('01002', '01000')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('^3630$', '03630')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('34521', '34500')
+        df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('94390', '94310')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('06900', '06000')
+        df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('85150', '85152')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('34935', '34000')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('80046', '80000')
         df_copy["Code Postal"] = df_copy["Code Postal"].str.replace('80083', '80000')
@@ -422,7 +465,7 @@ def show(new_df, mode, graph, details,distance):
             '''
             **Détails des formations:**
             '''
-            st.write(new_df)
+            st.dataframe(new_df)
 
 def carto_clean(new_df,counties, communes) :
     px.set_mapbox_access_token(open(".mapbox_token").read())
@@ -459,7 +502,7 @@ def show_graph_by_city(form_by_city):
     new_form_by_city['Nombre de formations']=pd.to_numeric(new_form_by_city['Nombre de formations'], errors='coerce')
 
     form_big_city = new_form_by_city.groupby(['Commune','Code Département']).agg({'Population':'sum','latitude':'median','longitude':'median','Nombre de formations':'sum'}).reset_index()
-    form_big_city = form_big_city[form_big_city['Population']>30000]
+    form_big_city = form_big_city[form_big_city['Population']>10000]
     form_big_city['Formations / 1000 Habitants']=form_big_city['Nombre de formations']/form_big_city["Population"]*1000
 
 
@@ -573,7 +616,7 @@ elif navigation=='Vue cartographique':
 
         form_big_city = show_graph_by_city(form_by_city)
 
-        st.write("Les données affichées ci-dessous correspondent aux formations proposées dans les **villes de plus de 30.000 habitants** sur l'app Mon Compte Formation pour les mot-clés listés (voir onglet Home pour la liste des mots-clés)")
+        st.write("Les données affichées ci-dessous correspondent aux formations proposées dans les **villes de plus de 10.000 habitants** sur l'app Mon Compte Formation pour les mot-clés listés (voir onglet Home pour la liste des mots-clés)")
 
         '''
         **Nombre de formations**
@@ -590,9 +633,9 @@ elif navigation=='Vue cartographique':
         st.plotly_chart(fig)
 
         '''
-        **Top des villes entre 40 et 80.000 habitants où le nombre de formations pour 1000 habitants est le plus faible**
+        **Top des villes de plus de 30.000 habitants où le nombre de formations pour 1000 habitants est le plus faible**
         '''
-        form_city_targeted = form_big_city[(form_big_city['Population']>40000) & (form_big_city['Population']<120000) ]
+        form_city_targeted = form_big_city[(form_big_city['Population']>30000) ]
         form_city_targeted['Inverse de Formations / 1000 Habitants'] = 1/form_city_targeted['Formations / 1000 Habitants']
         fig = px.scatter_mapbox(form_city_targeted.sort_values(by='Formations / 1000 Habitants'),lat="latitude", lon="longitude", size="Inverse de Formations / 1000 Habitants",
                   color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=3.7, hover_name='Commune')
