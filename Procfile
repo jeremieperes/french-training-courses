c05b6f214ba0a6d640eb833c2fe4ed223cc22c93
Jérémie Peres <jeremieperes@gmail.com>
2020-01-27 15:49:04 +0100

    Reorganize repo

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && streamlit run app/Formation-web-app.py
+web: sh setup.sh && streamlit run Formation-web-app.py
--- Windows_RUN.bat
+++
@@ -1,6 +0,0 @@
-cd %~dp0
-git clone https://github.com/jeremieperes/french-training-courses.git
-cd french-training-courses
-pip install -r requirements.txt
-cd app
-streamlit run Formation-web-app.py
